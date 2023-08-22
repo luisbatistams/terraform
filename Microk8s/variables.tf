6bebd7a5bd9af12de29e20d4caa4633c933c16e7
Luis Miguel Batista <lbatista@lbatistaJXJTF.vmware.com>
2023-08-22 15:11:05 -0300

    azure

--- Microk8s/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "kube_ip" {
-  description = "IP for Kubernetes"
-  default = "172.20.10.180:6443"
-}
-
-variable "nginx_node_port" {
-  description = "NGINX Node Port"
-  default = "30202"
-}
