d3cb27e270f782c699e2546469f42224c88c0b4d
Luis Miguel Batista <lbatista@lbatistaJXJTF.vmware.com>
2023-08-24 15:25:01 -0300

    vm-terraform

--- Azure/main.tf
+++ Azure/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_interface" "example" {
 }
 
 resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
+  name                  = "vm-terraform"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
